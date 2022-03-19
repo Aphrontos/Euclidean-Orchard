7cf9d5b00136470305c42dd1b8ba0063248b2896
Adiáphoros <47263323+Aphrontos@users.noreply.github.com>
2022-03-20 01:24:01 +0800

    Update generate.py
    Allowed the generation of colored PNGs
    Changed generate_data() so that the functions generates a different pattern

--- generate.py
+++ generate.py
@@ -1,76 +1,56 @@
-import zlib
-import struct
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 23:48:35 2022
+
+@author: USER
+"""
+
+import png
 import numpy
-from math import gcd
+from primesieve import nth_prime
+from math import log
 
 # Generate the data
 
-def generateData(data, height = None, width = None):
-    if height is None:
-        height = data.shape[0] # number of rows
-    if width is None:
-        width = data.shape[1] # number of columns
-				
+def generate_data(height = None, width = None):
+    p = 0
+    data = numpy.full((height, width), 0, dtype = int)
+    isMarked = numpy.full(width, False, dtype = bool)
     for j in range(height):
+        p = nth_prime(j + 1)
         for i in range(width):
-            if gcd(j, i) == 1:
+            if i % p != 0 or isMarked[i] :
                 data[j, i] = 0
+            elif i % p == 0  and i > 0:
+                data[j, i] = 1
+                isMarked[i] = True
+            
+    return data
 
-# Draheim, G. U. (2014, September 14). Creating a PNG file in Python. 
-#	Retrieved July 27, 2020, from https://stackoverflow.com/a/25835368
-# Generate the PNG
+# Gashler M. (2019, April 16) 
+#	Retrieved March 20 2022, from https://stackoverflow.com/a/55715162/15043016
+# Generate the png
 
-def generatePNG(data, name, height = None, width = None):
-    def I1(value):
-        return struct.pack("!B", value & (2**8-1))
-    def I4(value):
-        return struct.pack("!I", value & (2**32-1))
-    # compute width & height from data if not explicit
-    if height is None:
-        height = data.shape[0] # number of rows
-    if width is None:
-        width = data.shape[1] # number of columns
-    # generate these chunks depending on image type
-    makeIHDR = True
-    makeIDAT = True
-    makeIEND = True
-    png = b"\x89" + "PNG\r\n\x1A\n".encode('ascii')
-    if makeIHDR:
-        colortype = 0 # true gray image (no palette)
-        bitdepth = 8 # with one byte per pixel (0 to 255)
-        compression = 0 # zlib (no choice here)
-        filtertype = 0 # adaptive (each scanline seperately)
-        
-        interlaced = 0 # no
-        IHDR = I4(width) + I4(height) + I1(bitdepth)
-        IHDR += I1(colortype) + I1(compression)
-        IHDR += I1(filtertype) + I1(interlaced)
-        block = "IHDR".encode('ascii') + IHDR
-        png += I4(len(IHDR)) + block + I4(zlib.crc32(block))
-    if makeIDAT:
-        raw = b""
-        for y in range(height):
-            raw += b"\0" # no filter for this scanline
-            for x in range(width):
-                c = b"\0" # default black pixel
-                if y < len(data) and x < len(data[y]):
-                    c = I1(data[y][x])
-                raw += c
-        compressor = zlib.compressobj()
-        compressed = compressor.compress(raw)
-        compressed += compressor.flush() #!!
-        block = "IDAT".encode('ascii') + compressed
-        png += I4(len(compressed)) + block + I4(zlib.crc32(block))
-    if makeIEND:
-        block = "IEND".encode('ascii')
-        png += I4(0) + block + I4(zlib.crc32(block))
-    open(name + ".png","wb").write(png)
+def generate_png(data):
+    height = data.shape[0] # number of rows
+    width = data.shape[1] # number of columns
     
-height = input("height: ")
-width = input("width: ")
-name = input("name: ")
-
-data = numpy.full((height, width), 255, dtype = int)
-
-generateData(data, height, width)
-generatePNG(data, name, height, width)
+    img = []
+    for y in range(height):
+        row = ()
+        for x in range(width):
+            if data[y, x] == 0 :
+                row = row + (0x18, 0x46, 0x46)
+            elif data[y, x] == 1 :
+                row = row + (0x47, 0xEF, 0x91)
+        img.append(row)
+    with open('gradient.png', 'wb') as f:
+        w = png.Writer(width, height, greyscale=False)
+        w.write(f, img)
+    
+if __name__ == "__main__":
+    height = 10 # make sure this is small
+    width = round(height ** 2 * log(height))
+	
+    data = generate_data(height, width)
+    generate_png(data)
