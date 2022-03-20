6afb95bcfebe8c143087aac91ee2e65fd6b0320c
Adiáphoros <47263323+Aphrontos@users.noreply.github.com>
2022-03-20 19:36:14 +0800

    Update generate.py
    The primes themselves are now marked red
    The changed how the width is calculated

--- generate.py
+++ generate.py
@@ -2,55 +2,69 @@
 """
 Created on Sat Mar 19 23:48:35 2022
 
-@author: USER
+@author: Adíaphoros
 """
 
 import png
 import numpy
 from primesieve import nth_prime
-from math import log
+
+def get_primes(size):
+    primes = numpy.full(size, 0, dtype = int)
+    for i in range(size):
+        primes[i] = nth_prime(i+1)
+    
+    return primes
 
 # Generate the data
 
-def generate_data(height = None, width = None):
-    p = 0
+def generate_data(primes, height = None, width = None):
     data = numpy.full((height, width), 0, dtype = int)
     isMarked = numpy.full(width, False, dtype = bool)
+        
     for j in range(height):
-        p = nth_prime(j + 1)
+        p = primes[j]
         for i in range(width):
-            if i % p != 0 or isMarked[i] :
+            if   i % p != 0 or  isMarked[i]:
                 data[j, i] = 0
-            elif i % p == 0  and i > 0:
+            elif i % p == 0 and i > p:
                 data[j, i] = 1
                 isMarked[i] = True
+                
+    for j in range(height):
+        data[j, primes[j]] = 2
             
     return data
 
-# Gashler M. (2019, April 16) 
+# Gashler M. (2019, April 16) Creating a PNG file in Python
 #	Retrieved March 20 2022, from https://stackoverflow.com/a/55715162/15043016
 # Generate the png
 
 def generate_png(data):
     height = data.shape[0] # number of rows
-    width = data.shape[1] # number of columns
+    width  = data.shape[1] # number of columns
     
     img = []
     for y in range(height):
         row = ()
+        color = ()
         for x in range(width):
-            if data[y, x] == 0 :
-                row = row + (0x18, 0x46, 0x46)
-            elif data[y, x] == 1 :
-                row = row + (0x47, 0xEF, 0x91)
+            if   data[y, x] == 0:
+                color = (0x18, 0x46, 0x46)
+            elif data[y, x] == 1:
+                color = (0x47, 0xEF, 0x91)
+            elif data[y, x] == 2:
+                color = (0xFF, 0x00, 0x00)
+            row = row + color
         img.append(row)
-    with open('gradient.png', 'wb') as f:
+    with open('sieve.png', 'wb') as f:
         w = png.Writer(width, height, greyscale=False)
         w.write(f, img)
     
 if __name__ == "__main__":
-    height = 10 # make sure this is small
-    width = round(height ** 2 * log(height))
-	
-    data = generate_data(height, width)
+    height = 100 # make sure this is small
+    primes = get_primes(height+1)
+    width = primes[height] ** 2
+    
+    data = generate_data(primes, height, width)
     generate_png(data)
